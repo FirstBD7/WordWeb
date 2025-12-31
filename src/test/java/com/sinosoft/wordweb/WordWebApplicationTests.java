9cbf4a8a59ded02657c712b894751dd8e02532e4
lihao <1679249763@qq.com>
2025-12-31 19:04:39 +0800

    提交测试

---
+++ src/test/java/com/sinosoft/wordweb/WordWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinosoft.wordweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
